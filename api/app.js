65c7eab8c60f5aabe2d6dddd065205985b631ec1
Damoon Rashidi <damoon.rashidi@gmail.com>
2018-10-05 15:59:10 +0200

    Update app.js

--- api/app.js
+++ api/app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get('/idea', (req, res) => res.send('startup idea goes here'));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
