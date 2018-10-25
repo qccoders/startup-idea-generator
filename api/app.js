78be23eed1dac6ce4f3bac21a900d623a530fb89
JP Dillingham <jpdillingham@gmail.com>
2018-10-25 18:17:26 -0500

    add serverless stuff

--- api/app.js
+++ api/app.js
@@ -1,7 +1,18 @@
+const awsServerlessExpress = require('aws-serverless-express');  
+const awsServerlessExpressMiddleware = require('aws-serverless-express/middleware');
+const cors = require('cors');
+const bodyParser = require('body-parser'); 
+
 const startupList = require("./startups.js");
 const express = require("express");
 const nounsList = require("./nouns.js");
+
 const app = express();
+app.use(awsServerlessExpressMiddleware.eventContext());
+app.use(cors({ exposedHeaders: 'X-Total-Count' }));
+app.use(bodyParser.json());  
+app.use(bodyParser.urlencoded({ extended: true }));
+
 const port = 3000;
 
 const getStartupList = () => startupList.toString();
@@ -22,3 +33,7 @@ app.get("/startups", (req, res) => res.status(200).json(startupList));
 app.get("/nouns", (req, res) => res.status(200).json(nounsList));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+const server = awsServerlessExpress.createServer(app);
+
+exports.handler = (event, context) => awsServerlessExpress.proxy(server, event, context);
