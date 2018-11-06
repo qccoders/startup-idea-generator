6abed9771eae75378fcf1a8dbb8c250193a9fcdb
JP Dillingham <jpdillingham@gmail.com>
2018-11-05 21:29:10 -0600

    add fetch and display idea, closes #40

--- web/src/App.js
+++ web/src/App.js
@@ -3,13 +3,23 @@ import { Container, Header } from "semantic-ui-react";
 import axios from 'axios';
 
 class App extends Component {
-  render() {
-    return (
-      <Container text>
-        <Header as="h1" content="Startup Idea Generator" textAlign="center" />
-      </Container>
-    );
-  }
+    state = { idea: { startup: '', noun: '' } }
+
+    componentDidMount = () => {
+        axios.get('http://localhost:3001/api/idea')
+        .then(response => {
+            this.setState({ idea: response.data });
+        });
+    }
+
+    render() {
+        return (
+            <Container text>
+                <Header as="h1" content="Startup Idea Generator" textAlign="center" />
+                <span>{this.state.idea.startup}</span> for <span>{this.state.idea.noun}</span>
+            </Container>
+        );
+    }
 }
 
 export default App;
