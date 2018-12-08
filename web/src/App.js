513e748848a69f8a01e797eb2c6cd54225c43801
JP Dillingham <jpdillingham@gmail.com>
2018-12-08 17:38:38 -0600

    move fetch to get(), add button to fetch

--- web/src/App.js
+++ web/src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
-import { Container, Header } from "semantic-ui-react";
+import { Container, Header, Button } from "semantic-ui-react";
 import axios from 'axios';
 
 class App extends Component {
     state = { idea: { startup: '', noun: '' } }
 
     componentDidMount = () => {
+        this.get();
+    }
+
+    get = () => {
         axios.get('http://localhost:3001/api/idea')
         .then(response => {
             this.setState({ idea: response.data });
@@ -17,6 +21,8 @@ class App extends Component {
             <Container text>
                 <Header as="h1" content="Startup Idea Generator" textAlign="center" />
                 <span>{this.state.idea.startup}</span> for <span>{this.state.idea.noun}</span>
+                
+                <Button onClick={this.get}>Roll for a new idea</Button>
             </Container>
         );
     }
