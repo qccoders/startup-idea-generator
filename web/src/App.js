d26778c7a04b9875404c4e860e20ed887bee4e8d
JP Dillingham <jpdillingham@gmail.com>
2018-12-08 22:24:19 -0600

    styling

--- web/src/App.js
+++ web/src/App.js
@@ -5,58 +5,83 @@ import axios from 'axios';
 const styles = {
     for: {
         color: '#808080', 
-        'font-weight' : '100', 
-        'font-size': 24,
-        'margin-left': 5,
-        'margin-right': 5,
+        fontWeight : 100, 
+        fontSize: 24,
+        marginLeft: 5,
+        marginRight: 5,
+    },
+    info: {
+        display: 'inline-block',
+        fontSize: 15,
+        top: -25,
+        color: 'orangered',
+        position: 'relative',
     }
 }
 
 class App extends Component {
-    state = { startup: '', startupWiki: [], noun: '' }
+    state = { startup: '', startupWiki: [], showStartupWiki: false, noun: '' }
 
     componentDidMount = () => {
         this.get();
     }
 
     get = () => {
-        axios.get('http://localhost:3001/api/idea')
-        .then(response => {
-            this.setState({ startup: response.data.startup, noun: response.data.noun }, () => {
-                axios.get('https://en.wikipedia.org/w/api.php?action=opensearch&search=' + this.state.startup + '&format=json&limit=5&origin=*')
-                .then(response => {
-                    this.setState({ startupWiki: response.data });
+        this.setState({ showStartupWiki: false }, () => {
+            axios.get('http://localhost:3001/api/idea')
+            .then(response => {
+                this.setState({ startup: response.data.startup, noun: response.data.noun }, () => {
+                    axios.get('https://en.wikipedia.org/w/api.php?action=opensearch&search=' + this.state.startup + '&format=json&origin=*')
+                    .then(response => {
+                        this.setState({ startupWiki: response.data });
+                    });
                 });
             });
         });
     }
 
+    handleInfoClick = () => {
+        this.setState({ showStartupWiki: !this.state.showStartupWiki });
+    }
+
     render() {
-        let { startup, startupWiki, noun} = this.state;
+        let { startup, startupWiki, showStartupWiki, noun} = this.state;
 
         return (
-            <Container text textAlign='center' style={{'margin-top': 50}}>
+            <Container text textAlign='center' style={{ marginTop: 50}}>
                 <Header as="h3" icon textAlign="center">
                     <Icon name='idea' circular/>
                     Startup Idea Generator
                 </Header>
 
-                <Header as='h1' style={{ 'margin-top': 50 }}>
-                    {startup} <span style={styles.for}>for</span> {noun}
+                <Header as='h1' style={{ marginTop: 50 }}>
+                    <span>{startup}</span>
+                    <Icon 
+                        name='info circle' 
+                        style={styles.info}
+                        onClick={this.handleInfoClick}
+                    />
+                    <span style={styles.for}>for</span>
+                    <span>{noun}</span> 
                 </Header>
 
-                <Container text textAlign='left'>
+                {showStartupWiki && <Container text textAlign='left'>
                     <Segment raised>
-                        <ul>
-                            {startupWiki && startupWiki[1] && startupWiki[1].map((s, i) => <li>{s}: {startupWiki[2][i]}</li>)}
-                        </ul>
+                        {startupWiki && startupWiki[1] && 
+                            startupWiki[1].map((s, i) => 
+                                <Container key={i} style={{ marginBottom: 25 }}>
+                                    <Header as='h4'>{s}</Header>
+                                    <p>{startupWiki[2][i]}</p>
+                                </Container>
+                            )
+                        }
                     </Segment>
-                </Container>
+                </Container>}
 
                 <Button 
                     primary 
                     onClick={this.get}
-                    style={{'margin-top': 50}}
+                    style={{ marginTop: 50}}
                 >
                     Roll for a New Idea
                 </Button>
