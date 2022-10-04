044ed34ec58bc5efb2754a792082c40cca75c0ac
Eisen Uchiha <eisenuchiha@gmail.com>
2022-10-04 17:06:28 -0500

    Updated and migrated packages

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import "semantic-ui-css/semantic.min.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
